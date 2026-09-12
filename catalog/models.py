e6ff022414135b6fa15772cb374a045c22d82e0d
Ryan Barnett <ryandbarnett66@gmail.com>
2026-09-12 12:43:09 -0500

    Created and Applied migrations for catalog models.

--- catalog/models.py
+++ catalog/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Publisher(models.Model):
-    name = models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    pages = models.PositiveIntegerField()
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    pages=models.PositiveIntegerField()
+    publisher=models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 
 class Review(models.Model):
-    body = models.TextField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    body=models.TextField()
+    book=models.ForeignKey(Book, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Review of {self.book.title}"
